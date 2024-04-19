09d5cb935f43f0d7b0711161b3b75ee6f3fad7aa
Oliver Funk <oli.funk@gmail.com>
2024-04-19 21:45:16 +0100

    began modelling groups

--- packages/models/src/utils.ts
+++ packages/models/src/utils.ts
@@ -1,4 +1,4 @@
 import { Type as t, type TSchema } from "@sinclair/typebox";
 
-export const unsettable = <S extends TSchema>(setSchema: S) =>
+export const clearable = <S extends TSchema>(setSchema: S) =>
   t.Optional(t.Union([setSchema, t.Null()]));
--- packages/models/src/vibefire-group.ts
+++ packages/models/src/vibefire-group.ts
@@ -1,23 +1,41 @@
-import { Type as t, type Static } from "@sinclair/typebox";
+import { Type as t } from "@sinclair/typebox";
 
-import { CoordSchema, TimePeriodSchema } from "./general";
-import { unsettable } from "./utils";
+import { VibefireIndexableLocationSchema } from "./general";
 
-const VibefireGroupMembership = t.Object({
+export const VibefireGroupMembership = t.Object({
+  id: t.String({ default: undefined }),
   userId: t.String(),
-  dateJoinedUTC: t.String({ default: undefined }),
+  groupId: t.String(),
+  dateCreatedUTC: t.String({ default: undefined }),
   dateExpiresUTC: t.Optional(t.String({ default: undefined })),
-  subgroup: t.Optional(t.String({ default: "default" })),
-  isAdmin: t.Boolean({ default: false }),
 });
 
 export const VibefireGroup = t.Object({
+  id: t.String({ default: undefined }),
+  linkId: t.String({ default: undefined }),
+
+  ownerId: t.String({ default: undefined }),
+  ownerType: t.Union([t.Literal("user"), t.Literal("org")], {
+    default: undefined,
+  }),
+  managerIds: t.Array(t.String(), { default: [] }),
+
+  name: t.String({ default: undefined }),
+  description: t.String({ default: undefined }),
+  banner: t.String({ default: undefined }),
+
+  // location - used for discoverability of public groups
+  location: t.Optional(VibefireIndexableLocationSchema),
+
   // meta
   dateCreatedUTC: t.String({ default: undefined }),
   dateUpdatedUTC: t.String({ default: undefined }),
-
-  name: t.String({ default: undefined }),
-  linkId: t.String({ default: undefined }),
-
-  members: t.Array(VibefireGroupMembership, { default: [] }),
+  type: t.Union([
+    // discoverable by anyone in an area
+    t.Literal("public"),
+    // invite by members/link
+    t.Literal("private-joinable"),
+    // invite by owner/manager only
+    t.Literal("private-invite-only"),
+  ]),
 });
