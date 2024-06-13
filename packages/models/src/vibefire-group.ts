90944de117243ba0ce51ab400de9d688a73074ee
Oliver Funk <oli.funk@gmail.com>
2024-06-13 08:10:06 +0100

    prep'ing for massive refactor, everything works at this point

--- packages/models/src/vibefire-group.ts
+++ packages/models/src/vibefire-group.ts
@@ -1,9 +1,49 @@
 import { Type as t, type Static } from "@sinclair/typebox";
 
-import {
-  VibefireIndexableLocationSchema,
-  VibefireLocationSchema,
-} from "./general";
+import { VibefireLocationSchema } from "./general";
+
+// export const VibefireGroupPlanEntry = t.Object({
+//   entry: t.Union([
+//     t.Object({
+//       type: t.Literal("event"),
+//       eventId: t.String(),
+//       label: t.String({ maxLength: 100 }),
+//     }),
+//     t.Object({
+//       type: t.Literal("text"),
+//       description: t.String(),
+//       location: t.Optional(VibefireLocationSchema),
+//     }),
+//   ]),
+//   when: t.String({ format: "date-time" }),
+// });
+
+// export const VibefireGroupPlanComment = t.Object({
+//   byUserId: t.String(),
+//   comment: t.String({ maxLength: 1000 }),
+//   datePostedUTC: t.String({ format: "date-time" }),
+// });
+
+// export const VibefireGroupPlan = t.Object({
+//   id: t.String({ default: undefined }),
+//   groupId: t.String(),
+//   byUserId: t.String(),
+//   dateCreatedUTC: t.String({ default: undefined }),
+//   message: t.String({ maxLength: 1000 }),
+//   entries: t.Array(VibefireGroupPlanEntry, { default: [] }),
+//   comments: t.Array(VibefireGroupPlanComment, { default: [] }),
+//   // user ids of people who have said yes
+//   yesses: t.Array(t.String(), { default: [] }),
+// });
+
+// export const VibefireGroupSuggestion = t.Object({
+//   byUserId: t.String(),
+//   dateCreatedUTC: t.String({ default: undefined }),
+//   message: t.String({ maxLength: 1000 }),
+//   entry: VibefireGroupPlanEntry,
+//   // user ids of people who have said yes
+//   yesses: t.Array(t.String(), { default: [] }),
+// });
 
 export const VibefireGroupMembership = t.Object({
   id: t.String({ default: undefined }),
@@ -13,47 +53,8 @@ export const VibefireGroupMembership = t.Object({
   dateExpiresUTC: t.Optional(t.String({ default: undefined })),
 });
 
-export const VibefireGroupPlanEntry = t.Object({
-  entry: t.Union([
-    t.Object({
-      type: t.Literal("event"),
-      eventId: t.String(),
-      label: t.String({ maxLength: 100 }),
-    }),
-    t.Object({
-      type: t.Literal("text"),
-      description: t.String(),
-      location: t.Optional(VibefireLocationSchema),
-    }),
-  ]),
-  when: t.String({ format: "date-time" }),
-});
-
-export const VibefireGroupPlanComment = t.Object({
-  byUserId: t.String(),
-  comment: t.String({ maxLength: 1000 }),
-  datePostedUTC: t.String({ format: "date-time" }),
-});
-
-export const VibefireGroupPlan = t.Object({
-  id: t.String({ default: undefined }),
-  groupId: t.String(),
-  byUserId: t.String(),
-  dateCreatedUTC: t.String({ default: undefined }),
-  message: t.String({ maxLength: 1000 }),
-  entries: t.Array(VibefireGroupPlanEntry, { default: [] }),
-  comments: t.Array(VibefireGroupPlanComment, { default: [] }),
-  // user ids of people who have said yes
-  yesses: t.Array(t.String(), { default: [] }),
-});
-
-export const VibefireGroupSuggestion = t.Object({
-  byUserId: t.String(),
-  dateCreatedUTC: t.String({ default: undefined }),
-  message: t.String({ maxLength: 1000 }),
-  entry: VibefireGroupPlanEntry,
-  // user ids of people who have said yes
-  yesses: t.Array(t.String(), { default: [] }),
+const VibefireGroupImages = t.Object({
+  banners: t.Array(t.String(), { default: [] }),
 });
 
 export const VibefireGroup = t.Object({
@@ -71,49 +72,41 @@ export const VibefireGroup = t.Object({
 
   name: t.String({ default: undefined }),
   description: t.String({ default: undefined }),
-  imgKeys: t.Object({
-    banner: t.String({ default: undefined }),
-  }),
+  images: VibefireGroupImages,
 
-  socials: t.Optional(
-    t.Object({
-      twitter: t.Optional(t.String({ default: undefined })),
+  socials: t.Object(
+    {
+      x: t.Optional(t.String({ default: undefined })),
       facebook: t.Optional(t.String({ default: undefined })),
       instagram: t.Optional(t.String({ default: undefined })),
       tiktok: t.Optional(t.String({ default: undefined })),
       website: t.Optional(t.String({ default: undefined })),
-    }),
+    },
+    { default: {} },
   ),
 
-  // meta
-  dateCreatedUTC: t.String({ default: undefined }),
-  dateUpdatedUTC: t.String({ default: undefined }),
-  groupTypeSpecifics: t.Union([
+  specific: t.Union([
     t.Object({
       type: t.Literal("public"),
       // location - used for discoverability
       // possibly an array
-      location: t.Optional(VibefireIndexableLocationSchema),
+      location: t.Optional(VibefireLocationSchema),
     }),
     t.Object({
       type: t.Literal("private"),
-      joinType: t.Union([
+      access: t.Union([
         // invite by owner/managers only, requests require approval
         t.Literal("invite"),
         // all members can invite, requests require no approval
         t.Literal("open"),
       ]),
       // group code - used instead of requesting (for invite groups)
-      code: t.Optional(t.String({ default: undefined })),
-      // only private groups can have suggestions
-      suggestions: t.Array(VibefireGroupSuggestion, { default: [] }),
+      inviteCode: t.Optional(t.String({ default: undefined, maxLength: 6 })),
     }),
   ]),
+
+  // meta
+  dateCreatedUTC: t.String({ default: undefined }),
+  dateUpdatedUTC: t.String({ default: undefined }),
 });
-// // discoverable by anyone in an area
-// t.Literal("public"),
-// // invite by members/link, not discoverable
-// t.Literal("private-joinable"),
-// // invite by owner/manager only, not discoverable
-// t.Literal("private-invite-only"),
 export type VibefireGroupT = Static<typeof VibefireGroup>;
